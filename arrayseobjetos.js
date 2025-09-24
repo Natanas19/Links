ff65fa416543860c51e17b212bd004673da67a4b
Natanas19 <pro.natan98@gmail.com>
2025-09-23 22:07:39 -0300

    arrays e condicionais

---
+++ arrayseobjetos.js
@@ -0,0 +1,27 @@
+const alunosDoSenai = [
+    {
+        nome:"Natan",
+        email:"pro.natan98@gmail.com",
+        matricula:"123456",
+        idade:26
+    },
+    {
+        nome:"junior",
+        email:"jr@gmail,com",
+        matricula:"262626",
+        idade:23
+    },
+    {
+        nome:"junior",
+        email:"jr@gmail,com",
+        matricula:"262626",
+        idade:23
+    },
+    
+
+] 
+
+console.log(` Meu nome é ${alunosDoSenai[0].nome} 
+    e o meu email é ${alunosDoSenai[0].email} 
+    a minha matricula é ${alunosDoSenai[0].matricula} 
+    e a minha idade é ${alunosDoSenai[0].idade}`)  
