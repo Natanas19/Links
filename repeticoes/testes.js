c4fd2cdc42bc4f99dc36e2bcea3de7168d5c0a11
Natanas19 <pro.natan98@gmail.com>
2025-10-02 22:18:41 -0300

    a

---
+++ repeticoes/testes.js
@@ -0,0 +1,24 @@
+// let i = 0
+// while(i<3){
+//     alert("Valor do i: " + i)
+//     i++
+// }
+
+// for(let i = 0; i<3; i++){
+//     alert("Valor do i: " + i)}
+
+// let soma = 0
+// for(let i = 0; i<10; i++){
+//     let nota = Number(prompt("Digite a " + (i + 1) + "ª nota:"))
+//     soma = soma + nota
+// }
+
+// alert("soma " + soma) 
+
+// for(let i = 0; i<10; i++){
+//     alert("Quadrado de " (i) + "=" + (i * i))}
+ let i = 0
+ while(i<10){
+    i++
+    alert("Valor do i:" + i + "quadrado =" + (i*i))
+ }
